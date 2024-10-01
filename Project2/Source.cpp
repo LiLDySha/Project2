04f282850d24eb15657ab159203687232a4127e6
DySha <DySha@DESKTOP-2HEN19R>
2024-10-01 14:39:57 +0500

    gdfsgdfgd

--- Project2/Source.cpp
+++ Project2/Source.cpp
@@ -10,11 +10,42 @@ int main()
 	setlocale(LC_ALL, "ru");
 	srand(time(NULL));
 
-	int a = rand() % 5 + 5;
-	int arr[20]{};
+	const int size = 10;
+	int arr[size];
+	int mas[size];
 
-	std::cout << arr[5];
+	for (int i = 0; i < size; i++)
+	{
+		arr[i] = rand() % 10;
+	}
 
+	for (int i = 0; i < size; i++)
+	{
+		std::cout << "Введите числа в массив: ";
+		std::cin >> mas[i];
+	}
+
+	for (int i = 0; i < size; i++)
+	{
+		std::cout << "Массив рандомнович " << arr[i] << std::endl;
+	}
+
+	for (int i = 0; i < size; i++)
+	{
+		std::cout << "Массив вашевич " << mas[i] << std::endl;
+	}
+
+	for (int i = 0; i < size; i++)
+	{
+		if (mas[i] == arr[i])
+		{
+			std::cout << "\nТы удачник, в " << i <<  " номер попал";
+		}
+		else
+		{
+			std::cout << "\nНеудачник, ло... как там... ай не помню, того ты понял да? " << i;
+		}
+	}
 
 	return 0;
 }
